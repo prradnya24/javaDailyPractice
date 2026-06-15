8ebf28fc2947d35f90fd0be067e47a992cfd4714
prradnya24 <pradnyalate27@gmail.com>
2026-06-16 00:05:20 +0530

    write code

--- day1/first.java
+++ day1/first.java
@@ -1,8 +1,25 @@
 package day1;
+import java.util.*;
 
 public class first {
 
-    public static void public static void main(String[] args) {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int num;
+        num=sc.nextInt();
+
+        if(num == 0){
+            System.out.println("zero");
+
+        
+        }
+        if(num < 0){
+            System.out.println("Given num is negative");
+        }
+        else{
+            System.out.println("Given num is positive");
+        }
+
         
     }
     
